af40d44cf3d9d59955486cadeeda2fb6e90f009d
Abhimannu Singh Kunwar <155934864+Abhimannu09dev@users.noreply.github.com>
2024-04-18 07:59:06 +0545

    just cahnge come code adding background logo an panel

--- TeacherGUI.java
+++ TeacherGUI.java
@@ -3,23 +3,62 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import java.awt.Font;
 
 public class TeacherGUI{
-    JFrame frame;
+    JFrame TeacherGUI;
+    JLabel background,heading,Logo_,paragraph;
+    JPanel select;
+    ImageIcon back,Logo;
+    JButton Lecturer,Tutor,Quit;
     
+    //Creating constructor
     public TeacherGUI(){
-        frame= new JFrame("Teacher");
-        frame.setSize(1280, 823);
-        frame.setVisible(true);
-        frame.setLayout(null);
+        TeacherGUI = new JFrame();
         
-
-        JPanel panel = new JPanel();
-        frame.add(panel);
+        //Heading of JFrame
+        heading = new JLabel("Teacher Management System");// Creating the label for heading 
+        Font fhead = new Font("inter", Font.PLAIN, 32);//Declaring the font for heading
+        heading.setFont(fhead);//Setting the font of heading
+        heading.setBounds(410,71,477,47);//Setting the size of heading
+        TeacherGUI.add(heading);//adding the heading to teacherGUI
         
-        placeComponents(panel);
+        //Creating a background for Frame Teacher GUI
+        back = new ImageIcon(getClass().getResource("/background.png"));//Declaring image icon and calling the image  get resource helps to get the image
+        background = new JLabel(back);/*Decelaring JLabel background to set background of Frame 
+                                                        the back gives to the image*/
+        background.setBounds(0,0,1280,832);//Giving position for JLabel
+        TeacherGUI.add(background);//Adding JLAbel as background for frame
+        
+        //Creating ImageIcon and JLabel for Logo of college
+        Logo = new ImageIcon(getClass().getResource("/logo.png"));
+        Logo_ = new JLabel(Logo);
+        Logo_.setBounds(979,6,182,182);//Setting the size of logo
+        TeacherGUI.add(Logo_);//Adding the logo to frame
+        
+        //Creating JPanel for selection part
+        select = new JPanel();
+        select.setBounds(236,190,825,571);
+        TeacherGUI.add(select);
+        
+        //Creating the JLabel for selection part in JPanel
+        paragraph =  new JLabel("Please select the option of which the data want to be entered");
+        paragraph.setBounds(41,282,564,73);
+        select.add(paragraph);//Addding paragraph to panel
+        
+        //Creating Buttons for JPanel options
+        Lecturer = new JButton("Lecturer");
+        Tutor = new JButton("Tutor");
+        Quit = new JButton("Close");
+        
+        
+        //Setting various properties for Frame "TeacherGUI"
+        TeacherGUI.setTitle("TeacherGUI");
+        TeacherGUI.setLayout(null);
+        TeacherGUI.setVisible(true);
+        TeacherGUI.setSize(1280,832);
     }
-
     public static void main(String[] args){
         new TeacherGUI();
     }
