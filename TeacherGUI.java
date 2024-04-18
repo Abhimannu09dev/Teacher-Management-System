e945dc10f7f6e9fdbed8a92c0057556a5b91e924
Abhimannu Singh Kunwar <155934864+Abhimannu09dev@users.noreply.github.com>
2024-04-18 08:53:06 +0545

    some changes in Java

--- TeacherGUI.java
+++ TeacherGUI.java
@@ -24,33 +24,48 @@ public class TeacherGUI{
         heading.setBounds(410,71,477,47);//Setting the size of heading
         TeacherGUI.add(heading);//adding the heading to teacherGUI
         
-        //Creating a background for Frame Teacher GUI
-        back = new ImageIcon(getClass().getResource("/background.png"));//Declaring image icon and calling the image  get resource helps to get the image
-        background = new JLabel(back);/*Decelaring JLabel background to set background of Frame 
-                                                        the back gives to the image*/
-        background.setBounds(0,0,1280,832);//Giving position for JLabel
-        TeacherGUI.add(background);//Adding JLAbel as background for frame
-        
         //Creating ImageIcon and JLabel for Logo of college
         Logo = new ImageIcon(getClass().getResource("/logo.png"));
         Logo_ = new JLabel(Logo);
         Logo_.setBounds(979,6,182,182);//Setting the size of logo
         TeacherGUI.add(Logo_);//Adding the logo to frame
         
-        //Creating JPanel for selection part
-        select = new JPanel();
-        select.setBounds(236,190,825,571);
-        TeacherGUI.add(select);
+       //Creating JPanel for selection part
+       select = new JPanel();
+       select.setBounds(236,190,825,571);
+       TeacherGUI.add(select);//Adding JPanel to JFrame
+       
+
+       //Creating the JLabel for selection part in JPanel
+       paragraph =  new JLabel("Please select the option of which the data want to be entered");
+       Font fpara = new Font("inter", Font.PLAIN,20);//Declaring the font for paragraph
+       paragraph.setFont(fpara);//Settiing the  font for paragraph
+       paragraph.setBounds(41,282,564,73);
+       select.add(paragraph);//Addding paragraph to panel
+       
+       //Creating Buttons needed in JPanel options
+       Lecturer = new JButton("Lecturer");
+       Lecturer.setBounds(565,188,219,72);//Setting the size of Lecturer
+       select.add(Lecturer);//Addding Lecturer to panel
+       
+       Tutor = new JButton("Tutor");
+       Tutor.setBounds(565,377,219,72);//Setting size of Tutor
+       select.add(Tutor);//Adding to panel
+       
+       Quit = new JButton("Close");//Setting size of Quit
+       Quit.setBounds(237,484,219,72);
+       select.add(Quit);//Adding quit to panel
+       
+       select.setLayout(null);//Seting the layout of panel null
+
         
-        //Creating the JLabel for selection part in JPanel
-        paragraph =  new JLabel("Please select the option of which the data want to be entered");
-        paragraph.setBounds(41,282,564,73);
-        select.add(paragraph);//Addding paragraph to panel
         
-        //Creating Buttons for JPanel options
-        Lecturer = new JButton("Lecturer");
-        Tutor = new JButton("Tutor");
-        Quit = new JButton("Close");
+        //Creating a background for Frame Teacher GUI
+        back = new ImageIcon(getClass().getResource("/background.png"));//Declaring image icon and calling the image  get resource helps to get the image
+        background = new JLabel(back);/*Decelaring JLabel background to set background of Frame 
+                                                        the back gives to the image*/
+        background.setBounds(0,0,1280,832);//Giving position for JLabel
+        TeacherGUI.add(background);//Adding JLAbel as background for frame
         
         
         //Setting various properties for Frame "TeacherGUI"
